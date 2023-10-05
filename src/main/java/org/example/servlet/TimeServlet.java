5ae6c6c738dd78b0e6ccb55c607029e7f32ff70e
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-10-05 15:48:57 +0300

    refactoring

--- src/main/java/org/example/servlet/TimeServlet.java
+++ src/main/java/org/example/servlet/TimeServlet.java
@@ -1,36 +1,45 @@
 package org.example.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
-    private static final LocalDateTime DATE_TIME = LocalDateTime.now();
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final String GET_DATE_TIME = DATE_TIME.format(DATE_TIME_FORMATTER);
+
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=utf-8");
-        //resp.getWriter().write(GET_DATE_TIME);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
 
-        PrintWriter out = resp.getWriter();
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
 
-        // Зчитування HTML з файлу
-        try (BufferedReader reader = new BufferedReader(new FileReader("webapp\\time.html"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                out.println(line);
-            }
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        dateFormat.setTimeZone(timeZone);
+
+        String currentTime = dateFormat.format(new Date());
+
+        try (PrintWriter out = resp.getWriter()){
+            out.println("<!DOCTYPE html>");
+            out.println("<html lang=\"en\">");
+            out.println("<head>");
+            out.println("<title>Current Time</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Current Time</h1>");
+            out.println("<p>" + currentTime + "</p>");
+            out.println("</body>");
+            out.println("</html>");
         } catch (IOException e) {
-            e.printStackTrace();
-            out.println("An error occurred while reading the HTML file.");
+            throw new HtmlRenderingException("Error rendering HTML content", e);
         }
+
     }
 }
