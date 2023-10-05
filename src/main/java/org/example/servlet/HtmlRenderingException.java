6ebb68391b3175548e4f9b9fff92d55a03a48913
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-10-05 15:50:27 +0300

    refactoring

---
+++ src/main/java/org/example/servlet/HtmlRenderingException.java
@@ -0,0 +1,7 @@
+package org.example.servlet;
+
+public class HtmlRenderingException extends RuntimeException {
+    public HtmlRenderingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
